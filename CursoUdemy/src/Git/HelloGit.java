63cf216a32990164db97943c8bdba5ae552a8e6c
vinicius santos <vinic@DESKTOP-8289FFB>
2019-03-02 20:13:46 -0300

    add linha 1234

--- CursoUdemy/src/Git/HelloGit.java
+++ CursoUdemy/src/Git/HelloGit.java
@@ -8,6 +8,8 @@ public class HelloGit {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("1234");
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
@@ -22,6 +24,8 @@ public class HelloGit {
 		}
 		sc.close();
 		}
+	
+		
 
 	}
 
